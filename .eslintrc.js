3528a9a072b5a9c048f156c1e07398a09159976f
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-02-15 13:57:23 -0500

    chore: add astro files to eslint and tailwind configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,52 @@
 module.exports = {
-    root: true,
-    env: {
-        browser: true,
-        es6: true,
-        node: true,
-    },
-    extends: [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:import/recommended",
-        "plugin:import/warnings",
-        "plugin:import/typescript",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "next/core-web-vitals"
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:astro/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  ignorePatterns: [
+    "dist", // Ignore built files.
+    "out",
+    "build",
+    "node_modules",
+  ],
+  plugins: ["@typescript-eslint", "import"],
+  rules: {
+    "import/no-unresolved": 0,
+    "import/order": [
+      "warn",
+      {
+        groups: ["builtin", "external", "internal", "parent", "sibling", "index"],
+      },
     ],
-    parser: "@typescript-eslint/parser",
-    ignorePatterns: [
-        "dist", // Ignore built files.
-        "out",
-        "build",
-        "node_modules",
-    ],
-    plugins: ["react", "@typescript-eslint", "import"],
-    rules: {
-        "import/no-unresolved": 0,
-        "import/order": [
-            "warn",
-            {
-                groups: [
-                    "builtin",
-                    "external",
-                    "internal",
-                    "parent",
-                    "sibling",
-                    "index",
-                ],
-            },
-        ],
+  },
+  overrides: [
+    {
+      // Define the configuration for `.astro` file.
+      files: ["*.astro"],
+      // Allows Astro components to be parsed.
+      parser: "astro-eslint-parser",
+      // Parse the script in `.astro` as TypeScript by adding the following configuration.
+      // It's the setting you need when using TypeScript.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {
+        // override/add rules settings here, such as:
+        // "astro/no-set-html-directive": "error"
+      },
     },
-};
+  ],
+}
