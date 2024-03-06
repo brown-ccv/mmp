571ec0bdf39c7fe2bdcf021c9ccf54a892530743
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-03-06 12:25:11 -0500

    chore: site title and description for mmp

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = "Astro Blog"
-export const SITE_DESCRIPTION = "Welcome to my website!"
+export const SITE_TITLE = "MMP"
+export const SITE_DESCRIPTION = "Mesoamerican Migration Project"
 
 export const LINKS = ["news", "publications", "map", "data", "documentation", "about"]
