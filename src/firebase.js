0cd1fa8b3ecfe2666a8fe53626a7355d1fb2abb0
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-03-07 16:06:37 -0500

    feat: add authenticate with email link
    also includes linking to firestore (if new user, add them to firestore)

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,11 @@
 import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
+import { getFirestore, doc, getDoc, getDocs, collection, setDoc } from "firebase/firestore"
+import {
+  getAuth,
+  sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
+} from "firebase/auth"
 import firebaseConfig from "./firebase-config.json"
 
 // Initialize Firebase
@@ -7,3 +13,65 @@ const app = initializeApp(firebaseConfig)
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app)
+const auth = getAuth()
+const user = auth.currentUser
+
+// Magic link for auth
+const actionCodeSettings = {
+  // URL you want to redirect back to. The domain (www.example.com) for this
+  // URL must be in the authorized domains list in the Firebase Console.
+  url: "https://mmp-site-b1c9b.web.app/finishSignUp",
+  // This must be true.
+  handleCodeInApp: true,
+  iOS: {
+    bundleId: "com.example.ios",
+  },
+  android: {
+    packageName: "com.example.android",
+    installApp: true,
+    minimumVersion: "12",
+  },
+  dynamicLinkDomain: "example.page.link",
+}
+
+export const addUserFirestore = async (user) => {
+  // check if user exists; if not, add them to db
+  const userRef = doc(db, "users", user.uid)
+  const userSnap = await getDoc(userRef)
+  if (!userSnap.exists()) {
+    await setDoc(doc(db, "users", user.uid), {
+      email: user.email,
+    })
+  }
+}
+
+// Check the link is a sign-in with email link
+if (isSignInWithEmailLink(auth, window.location.href)) {
+  // Additional state parameters can also be passed via URL.
+  // This can be used to continue the user's intended action before triggering
+  let email = window.localStorage.getItem("emailForSignIn")
+  if (!email) {
+    // User opened the link on a different device. To prevent session fixation
+    // attacks, ask the user to provide the associated email again. For example:
+    email = window.prompt("Please provide your email for confirmation")
+  }
+  // The client SDK will parse the code from the link for you.
+  try {
+    const result = await signInWithEmailLink(auth, email, window.location.href)
+    window.localStorage.removeItem("emailForSignIn")
+    if (result.additionalUserInfo.isNewUser) {
+      await addUserFirestore(result.user)
+    }
+  } catch (error) {
+    return error.message
+  }
+}
+export const sendAuthenticateLink = async (email) => {
+  try {
+    const result = await sendSignInLinkToEmail(auth, email, actionCodeSettings)
+    window.localStorage.setItem("emailForSignIn", email)
+    return result
+  } catch (error) {
+    return error.message
+  }
+}
