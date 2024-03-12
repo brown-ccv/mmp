d91b37edc3c4d71039c1d5d3f48ea9201f984fb4
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-03-12 16:29:43 -0400

    refactor: group tw styles

--- src/components/DownloadModal.tsx
+++ src/components/DownloadModal.tsx
@@ -10,7 +10,7 @@ const DownloadModal = () => {
   return (
     <Dialog.Root open={isOpen} onOpenChange={setIsOpen}>
       <Dialog.Trigger asChild>
-        <button className="sm:inline-flex items-center justify-center text-gray-500 w-8 h-8 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 rounded-lg text-sm">
+        <button className="sm:inline-flex items-center justify-center w-8 h-8 rounded-lg text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200">
           <DownloadIcon />
           <span className="sr-only">Download data</span>
         </button>
@@ -21,9 +21,9 @@ const DownloadModal = () => {
           onClick={() => setIsOpen(false)}
         />
         <Dialog.Overlay className="fixed top-0 left-0 right-0 bottom-0 grid place-items-center z-10 w-screen overflow-y-auto">
-          <Dialog.Content className="rounded-lg text-left shadow-xl max-h-fit transition-all sm:my-8 sm:w-full sm:max-w-lg bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+          <Dialog.Content className="rounded-lg text-left shadow-xl max-h-fit transition-all bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4 sm:my-8 sm:w-full sm:max-w-lg">
             <div className="flex justify-end">
-              <Dialog.Close className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center">
+              <Dialog.Close className="text-gray-400 bg-transparent  rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center hover:bg-gray-200 hover:text-gray-900">
                 <Cross2Icon />
                 <span className="sr-only">Close</span>
               </Dialog.Close>
@@ -38,7 +38,7 @@ const DownloadModal = () => {
               </div>
               <button
                 type="submit"
-                className="flex items-center gap-2 bg-black text-white  hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                className="flex items-center gap-2 rounded-lg px-5 py-2.5 bg-black text-white text-sm text-center font-medium hover:bg-gray-500 focus:ring-4 focus:outline-none focus:ring-gray-500"
               >
                 <PlusIcon />
                 <span className="pt-1">Validate Email</span>
