d66b32bd17fe968825a94a84a85da0152b4f7475
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-03-21 12:50:32 -0400

    test: comment out window storage use
    need to think about this

--- src/firebase.js
+++ src/firebase.js
@@ -47,27 +47,27 @@ export const addUserFirestore = async (user) => {
   }
 }
 
-// Check the link is a sign-in with email link
-if (isSignInWithEmailLink(auth, window.location.href)) {
-  // Additional state parameters can also be passed via URL.
-  // This can be used to continue the user's intended action before triggering
-  let email = window.localStorage.getItem("emailForSignIn")
-  if (!email) {
-    // User opened the link on a different device. To prevent session fixation
-    // attacks, ask the user to provide the associated email again. For example:
-    email = window.prompt("Please provide your email for confirmation")
-  }
-  // The client SDK will parse the code from the link for you.
-  try {
-    const result = await signInWithEmailLink(auth, email, window.location.href)
-    window.localStorage.removeItem("emailForSignIn")
-    if (result.additionalUserInfo.isNewUser) {
-      await addUserFirestore(result.user)
-    }
-  } catch (error) {
-    return error.message
-  }
-}
+// // Check the link is a sign-in with email link
+// if (isSignInWithEmailLink(auth, window.location.href)) {
+//   // Additional state parameters can also be passed via URL.
+//   // This can be used to continue the user's intended action before triggering
+//   let email = window.localStorage.getItem("emailForSignIn")
+//   if (!email) {
+//     // User opened the link on a different device. To prevent session fixation
+//     // attacks, ask the user to provide the associated email again. For example:
+//     email = window.prompt("Please provide your email for confirmation")
+//   }
+//   // The client SDK will parse the code from the link for you.
+//   try {
+//     const result = await signInWithEmailLink(auth, email, window.location.href)
+//     window.localStorage.removeItem("emailForSignIn")
+//     if (result.additionalUserInfo.isNewUser) {
+//       await addUserFirestore(result.user)
+//     }
+//   } catch (error) {
+//     throw new Error(error)
+//   }
+// }
 export const sendAuthenticateLink = async (email) => {
   try {
     const result = await sendSignInLinkToEmail(auth, email, actionCodeSettings)
