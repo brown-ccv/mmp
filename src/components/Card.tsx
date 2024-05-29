763e8a78e8f4f8382ae92f2359eb215d5f125fbd
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-05-29 13:11:58 -0400

    style: match figma

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,14 +1,24 @@
 import React from "react"
 
 interface CardProps {
+  position: number
   image?: string
   title: string
   name: string
+  address?: string
+  phone?: string
+  email?: string
 }
 
-const Card: React.FC<CardProps> = ({ image, title, name }) => {
+const Card: React.FC<CardProps> = ({ position, image, title, name, address, phone, email }) => {
   return (
-    <div className="flex flex-row items-center">
+    <div
+      className={
+        position % 2
+          ? "flex flex-row items-center flex-row-reverse text-right"
+          : "flex flex-row items-center"
+      }
+    >
       {image && (
         <div>
           <img
@@ -19,8 +29,19 @@ const Card: React.FC<CardProps> = ({ image, title, name }) => {
         </div>
       )}
       <div className="px-8">
-        <p>{name}</p>
-        <p>{title}</p>
+        <div>
+          <p className="text-xl font-semibold">{name}</p>
+          <p className="italic">{title}</p>
+        </div>
+        <div>
+          {address && <p className="text-base">{address}</p>}
+          {phone && <p className="text-base">{phone}</p>}
+          {email && (
+            <a className="text-base hover:text-neutral-300" href={`mailto:${email}`}>
+              {email}
+            </a>
+          )}
+        </div>
       </div>
     </div>
   )
