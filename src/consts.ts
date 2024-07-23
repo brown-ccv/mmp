9f94fc2d8e39a54f05b2d8ddfc47f1206b9f3932
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-07-23 09:42:56 -0400

    fix: list of pages in const update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
 export const SITE_TITLE = "MMP"
 export const SITE_DESCRIPTION = "Mesoamerican Migration Project"
 
-
-export const LINKS = ["people", "news", "publications", "map", "data", "documentation"]
+export const LINKS = ["people", "news", "publications", "study-design", "data", "documentation"]
