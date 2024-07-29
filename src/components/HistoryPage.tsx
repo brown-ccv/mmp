5c3527cb6337c38ee037e1b354a3478479085f8a
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-07-29 13:05:10 -0400

    feat: History Page

---
+++ src/components/HistoryPage.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react"
+import type { UserInfo } from "firebase/auth"
+import Login from "../components/Login"
+import HistoryTable from "../components/HistoryTable"
+import { getHistoryData } from "../firebase"
+
+const HistoryPage = () => {
+  const [user, setUser] = useState<UserInfo | null | undefined>(null)
+  const [historyData, setHistoryData] = useState<any[] | null>(null)
+  const setUserFunction = (loggedUser: UserInfo | null | undefined) => {
+    setUser(loggedUser)
+  }
+
+  useEffect(() => {
+    const getData = async () => {
+      return await getHistoryData()
+    }
+
+    if (user) {
+      getData().then((data) => {
+        setHistoryData(data)
+      })
+    }
+  }, [user])
+
+  return (
+    <div className="space-y-8">
+      <section>
+        <div className="flex justify-end">
+          <Login currentUser={user} setUserFunction={setUserFunction} />
+        </div>
+        {!user && (
+          <p>
+            This section of the website is reserved for administrators to view download statistics.
+          </p>
+        )}
+      </section>
+      {historyData && (
+        <section>
+          <HistoryTable data={historyData} />
+        </section>
+      )}
+    </div>
+  )
+}
+
+export default HistoryPage
