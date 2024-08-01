d51f5157652ae45198fb7c6c2f3325e97f3d7c55
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-01 11:41:39 -0400

    feat: add text counter for textarea

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import * as Form from "@radix-ui/react-form"
 
 interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
@@ -7,20 +7,30 @@ interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement
 }
 
 export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ name, label, ...delegated }, ref) => (
-    <Form.Field name={name} className="flex flex-col gap-2">
-      <Form.Label>{label}</Form.Label>
-      <Form.Control asChild>
-        <textarea
-          rows={4}
-          className="text-gray-400 text-sm font-medium outline-none border-b-2 w-full"
-          {...delegated}
-          ref={ref}
-        />
-      </Form.Control>
-      <Form.Message className="text-primary-300" match="valueMissing">
-        Please enter your {label}
-      </Form.Message>
-    </Form.Field>
-  )
+  ({ name, label, ...delegated }, ref) => {
+    const [characterCount, setCharacterCount] = useState(0)
+    const maxLength = 300
+
+    return (
+      <Form.Field name={name} className="relative flex flex-col gap-2">
+        <Form.Label>{label}</Form.Label>
+        <span className="px-2 py-1 text-xs text-neutral-300 rounded absolute top-9 right-1">
+          {characterCount}/{maxLength}
+        </span>
+        <Form.Control asChild>
+          <textarea
+            rows={4}
+            maxLength={maxLength}
+            className="text-gray-400 text-sm font-medium outline-none border-b-2 w-full py-6"
+            {...delegated}
+            ref={ref}
+            onChange={(e) => setCharacterCount(e.target.value.length)}
+          />
+        </Form.Control>
+        <Form.Message className="text-primary-300" match="valueMissing">
+          Please enter your {label}
+        </Form.Message>
+      </Form.Field>
+    )
+  }
 )
