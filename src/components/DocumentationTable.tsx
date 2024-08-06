86018b79f0a3ae33d6d30992b5e35d473d3011ff
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-06 14:01:07 -0400

    feat: en/es versions of files

--- src/components/DocumentationTable.tsx
+++ src/components/DocumentationTable.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react"
-import Switch from "./Switch.tsx"
+import React from "react"
 
 interface DocumentationTableProps {
   allFiles: {
@@ -7,7 +6,7 @@ interface DocumentationTableProps {
       title: string
       cat: string
       file?: string
-      archivo: string
+      archivo?: string
       description?: string
       version?: string
     }
@@ -19,14 +18,12 @@ const DocumentationTable: React.FC<DocumentationTableProps> = ({ allFiles, versi
   const files = allFiles.map((file) => {
     return { ...file.data }
   })
-  const [language, setLanguage] = useState("English")
   return (
     <div className="w-full overflow-x-scroll no-scrollbar">
-      <Switch name="language" option1="English" option2="Spanish" updateSelection={setLanguage} />
-      <table className="table-fixed border-spacing-2">
+      <table className="table-fixed border-spacing-2 w-full">
         <thead>
           <tr className="bg-neutral-100 text-left text-neutral-900">
-            <th className="flex items-center w-[200px]">File</th>
+            <th className="flex items-center w-1/4">File</th>
             <th>Description</th>
             {version && <th>Field Season</th>}
           </tr>
@@ -36,14 +33,31 @@ const DocumentationTable: React.FC<DocumentationTableProps> = ({ allFiles, versi
           {files.map(({ title, file, description, version, archivo }, i) => {
             return (
               <tr key={i}>
-                <td>
-                  <a
-                    className="text-secondary-blue-700 hover:text-secondary-blue-500"
-                    target="_blank"
-                    href={language === "English" && file ? file : archivo}
-                  >
-                    {title}
-                  </a>
+                <td className="flex justify-between gap-2">
+                  <p className="font-bold">{title}</p>
+                  <div className="flex">
+                    (
+                    {file && (
+                      <a
+                        className="text-secondary-blue-700 hover:text-secondary-blue-500"
+                        target="_blank"
+                        href={file}
+                      >
+                        en
+                      </a>
+                    )}
+                    {file && archivo && <p>/</p>}
+                    {archivo && (
+                      <a
+                        className="text-secondary-blue-700 hover:text-secondary-blue-500"
+                        target="_blank"
+                        href={archivo}
+                      >
+                        es
+                      </a>
+                    )}
+                    )
+                  </div>
                 </td>
                 <td>{description}</td>
                 {version && <td>{version}</td>}
