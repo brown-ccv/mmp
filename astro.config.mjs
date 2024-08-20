940a8b5fdad9914f55f213beb72d949b9642d160
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-20 13:16:23 -0400

    chore: node ssr rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  output: "hybrid",
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
