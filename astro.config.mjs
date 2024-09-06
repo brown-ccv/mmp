17503fe2678617df85923470d1baa3a379385d26
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-09-06 12:01:39 -0400

    chore: enable srr with node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), react(), auth()]
+  output: "hybrid",
+  integrations: [mdx(), sitemap(), tailwind(), react(), auth()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
