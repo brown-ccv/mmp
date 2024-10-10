f473a5f9929d423a28fe2c024247a80c153a8911
bradford roarr <bradford.barr@gmail.com>
2024-10-10 17:02:28 -0400

    tried new astro integration, github logs in but decap fails. looks like the first calls to github after the auth fail, but it remakes them and the second time they succeed. It's close but needs extra eyes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,20 @@ import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 import react from "@astrojs/react"
 import node from "@astrojs/node"
-import auth from "auth-astro"
+
+import decapCmsOauth from "astro-decap-cms-oauth"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
   output: "hybrid",
-  integrations: [mdx(), sitemap(), tailwind(), react(), auth()],
+  integrations: [
+    decapCmsOauth({ decapCMSVersion: "3.3.3" }),
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+  ],
   adapter: node({
     mode: "standalone",
   }),
